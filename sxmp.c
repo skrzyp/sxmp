79a12f9ff2b57124f06b50600655ad8566f73a10
Jakub Skrzypnik <j.skrzypnik@openmailbox.org>
2017-01-21 15:59:04 +0100

    pulseaudio version

--- sxmp.c
+++ sxmp.c
@@ -13,7 +13,11 @@ void version(void){
 
 int main(int argc, char **argv) {
   version();
-
+  if (argv[1] == 0) {
+    printf("usage, %s <filename>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  int error;
   xmp_context context = xmp_create_context();
   struct xmp_frame_info frame_info;
   static const pa_sample_spec simple_spec = {
@@ -22,26 +26,31 @@ int main(int argc, char **argv) {
       .channels = 2
   };
   pa_simple *stream = NULL;
-  stream = pa_simple_new(NULL, argv[0], PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL);
+  stream = pa_simple_new(NULL, argv[0], PA_STREAM_PLAYBACK, NULL, "playback", &simple_spec, NULL, NULL, &error);
 
-  if (xmp_load_module(xmpcon, "test.mod") != 0){
-    fprintf(stderr, "can't find test.mod\n");
+  if (xmp_load_module(context, argv[1]) != 0){
+    fprintf(stderr, "can't find %s\n", argv[1]);
     exit(EXIT_FAILURE);
   }
 
-  xmp_start_player(xmpcon, 44100, 0);
-
-  while (xmp_play_frame(xmpcon) == 0) {
-    xmp_get_frame_info(xmpcon, &frameinfo);
-    if (frameinfo.loop_count > 0)
-      break;
-    pa_simple_write(stream, frameinfo.buffer, frameinfo.buffer_size);
+  if (xmp_test_module(argv[1], NULL) != 0) {
+    fprintf(stderr, "%s is not a valid module\n", argv[1]);
+    exit(EXIT_FAILURE);
   }
 
-  pa_simple_drain(stream);
-  xmp_end_player(xmpcon);
-  xmp_release_module(xmpcon);
-  xmp_free_context(xmpcon);
+  xmp_start_player(context, 44100, 0);
+
+  while (xmp_play_frame(context) == 0) {
+    xmp_get_frame_info(context, &frame_info);
+    if (frame_info.loop_count > 0)
+      break;
+    pa_simple_write(stream, frame_info.buffer, frame_info.buffer_size, &error);
+  }
+
+  pa_simple_drain(stream, &error);
+  xmp_end_player(context);
+  xmp_release_module(context);
+  xmp_free_context(context);
 
   return 0;
 }
